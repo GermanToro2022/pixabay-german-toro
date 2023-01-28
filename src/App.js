bbe86d6d1ad72a73940e3e35eda4d9772f951d52
GermanToro2022 <germanjavier19@gmail.com>
2023-01-27 22:04:54 -0300

    buscador de imagenes con javascript y react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    termino: '',
+    imagenes: [],
+    pagina: ''
+  }
+
+  paginaAnterior = () => {
+    // leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    // si la pagina es 1 no volver hacia atras
+    if(pagina === 1) return null;
+
+    // restar uno a la pagina actual 
+    pagina -= 1;
+  
+    // agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+    this.consultarApi();
+    });
+  
+    // console.log(pagina);
+  }
+
+  paginaSiguiente = () => {
+  // leer el state de la pagina actual
+  let pagina = this.state.pagina;
+
+  // sumar uno a la pagina actual 
+  pagina += 1;
+
+  // agregar el cambio al state
+  this.setState({
+    pagina
+  }, () => {
+    this.consultarApi()
+  });
+
+  // console.log(pagina);
+  }
+
+
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=32979334-60b1f78c3c6e743863dd2275d&q=${termino}&
+    page=${pagina}`;
+
+    console.log(url)
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({ imagenes: resultado.hits }))
+    
+
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino : termino,
+      pagina : 1
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de Imagenes</p>
+
+          <Buscador
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        <div className='row justify-content-center'>
+          <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+
+
 
 export default App;
---
+++ src/componentes/Buscador.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Buscador extends Component {
+
+    busquedaRef = React.createRef();
+
+    obtenerDatos = (e) => {
+    e.preventDefault();
+    
+    // Tomamos el valor del input
+    const termino = this.busquedaRef.current.value;
+
+    // Lo enviamos al componente principal    
+    this.props.datosBusqueda(termino);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.obtenerDatos}>
+                <div className='row'>
+                    <div className='form-group col-md-8'>
+                        <input ref={this.busquedaRef} type="text" className='form-control form-control-lg' placeholder='Busca tu imagen. Ejemplo: Fútbol' />
+                    </div>
+                    <div className='row'>
+                        <div className='form-group col-md-5'>
+                            <input type="submit" className="btn-lg btn-danger btn-block" value="Buscar..." />
+                        </div>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+};
+
+export default Buscador; 
